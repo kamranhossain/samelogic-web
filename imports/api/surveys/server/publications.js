8c1f91538d29c4c0a7ca476e0baf2b3f846d17ca
Shawn Mclean <shawnrmclean@gmail.com>
2016-03-29 20:00:48 -0500

    Validation added to get survey publication

--- imports/api/surveys/server/publications.js
+++ imports/api/surveys/server/publications.js
@@ -1,9 +1,11 @@
 import { Meteor } from 'meteor/meteor'
-
+import { SimpleSchema } from 'meteor/aldeed:simple-schema'
 import { Surveys } from '../surveys'
 
 Meteor.publish('survey.public', (surveyId) => {
-    //TODO: Add simpleschema validation of surveyId here.
+    new SimpleSchema({
+        surveyId: { type: String }
+    }).validate({surveyId})
     
     const query = {
         _id: surveyId
