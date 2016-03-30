6b826bb81b6c77d3fa2d094ac11e3b46847b9f6c
Shawn Mclean <shawnrmclean@gmail.com>
2016-03-30 00:14:06 -0500

    Updated survey methods.

--- imports/api/surveys/methods.js
+++ imports/api/surveys/methods.js
@@ -1,5 +1,8 @@
+import { Meteor } from 'meteor/meteor'
 import { ValidatedMethod } from 'meteor/mdg:validated-method'
 import { SimpleSchema } from 'meteor/aldeed:simple-schema'
+import { DDPRateLimiter } from 'meteor/ddp-rate-limiter'
+import { _ } from 'meteor/underscore'
 
 import { Surveys } from './surveys'
 
@@ -20,3 +23,20 @@ export const create = new ValidatedMethod({
         Surveys.insert(survey)
     }
 })
+
+// Get list of all method names on Surveys
+const METHODS = _.pluck([
+    create
+], 'name')
+
+if (Meteor.isServer) {
+    // Only allow 5 survey operations per connection per second
+    DDPRateLimiter.addRule({
+        name(name) {
+            return _.contains(METHODS, name)
+        },
+
+        // Rate limit per connection ID
+        connectionId() { return true }
+    }, 5, 1000)
+}
