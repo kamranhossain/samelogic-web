84d5507c4b1537590a71e5f6faf0647e7795630f
Shawn Mclean <shawnrmclean@gmail.com>
2016-04-03 02:19:30 -0500

    Added logic on client to update the video response with aws url.

--- imports/ui/admin/app/AdminAppContainer.jsx
+++ imports/ui/admin/app/AdminAppContainer.jsx
@@ -1,8 +1,9 @@
 import React,{Component} from 'react'
+import {Tracker} from 'meteor/tracker'
 
 import { Slingshot } from 'meteor/edgee:slingshot'
 
-import { createVideoResponse } from '/imports/api/surveys/methods'
+import { createVideoResponse, updateVideoResponseUrl } from '/imports/api/surveys/methods'
 
 export default class AdminAppContainer extends Component{    
     constructor(props){
@@ -26,6 +27,8 @@ export default class AdminAppContainer extends Component{
     }
     
     upload(event){
+        let self = this
+        
         const responseId = createVideoResponse.call({
             surveyId: 'test'
         }, (err, resp) =>{
@@ -37,14 +40,28 @@ export default class AdminAppContainer extends Component{
         })
         
         const uploader = new Slingshot.Upload( 'uploadSurveyVideo', {surveyResponseId: responseId} )
-
+        let computation = Tracker.autorun(function () {
+            self.setState({progress: Math.ceil(uploader.progress()) * 100})
+        })
         uploader.send( event.target.files[0], ( err, url ) => {
+            computation.stop()
             if ( err ) {
                 console.error('aws error: '+ err)
             } else {
+                updateVideoResponseUrl.call({
+                    surveyResponseId: responseId,
+                    url: url
+                }, (err, resp) =>{
+                    if ( err ) {
+                        console.error('mongo update error: '+ err)
+                    } else {
+                        console.log('mongo update success: '+ resp)
+                    }
+                })
                 console.log('aws success: '+ url)
             }
         })
+    
     }
     
     render(){
@@ -52,8 +69,9 @@ export default class AdminAppContainer extends Component{
             <div>            
                 <h1>File Upload</h1>
                 <form id="upload">
-                    <input type="file" onChange={this.upload} />
+                    <input type="file" onChange={this.upload.bind(this)} />
                 </form>
+                <span cssClass="sr-only">{this.state.progress}% Complete</span>
             </div>
         )
     }
