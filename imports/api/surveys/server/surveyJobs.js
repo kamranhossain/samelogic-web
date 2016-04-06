3ad1cebf3d4cad1379089897191f6925aea3ec76
Shawn Mclean <shawnrmclean@gmail.com>
2016-04-06 01:19:53 -0500

    Added quick hack for processing `startVideoAnalysis` job.

--- imports/api/surveys/server/surveyJobs.js
+++ imports/api/surveys/server/surveyJobs.js
@@ -3,3 +3,11 @@ import { JobCollection }from 'meteor/vsivsi:job-collection'
 var jc = JobCollection('surveyJobQueue')
 
 jc.startJobServer()
+
+jc.processJobs('startVideoAnalysis', (job, callback) =>{
+    console.log(job.data)
+    job.done()
+    callback()
+})
+
+export default jc
