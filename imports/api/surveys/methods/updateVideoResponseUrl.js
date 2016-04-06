e9840c58044b4c8f91adc8f73ba6d28ead24b2fb
Shawn Mclean <shawnrmclean@gmail.com>
2016-04-06 00:54:25 -0500

    Completion of video upload method creates a `startVideoAnalysis` job.

--- imports/api/surveys/methods/updateVideoResponseUrl.js
+++ imports/api/surveys/methods/updateVideoResponseUrl.js
@@ -1,5 +1,6 @@
 import { ValidatedMethod } from 'meteor/mdg:validated-method'
 import { SimpleSchema } from 'meteor/aldeed:simple-schema'
+import { Job }from 'meteor/vsivsi:job-collection'
 
 import { SurveyResponses } from '../collections/surveyResponses'
 
@@ -14,6 +15,17 @@ export default new ValidatedMethod({
     }).validator(),
     run({ surveyResponseId, url}) {
         SurveyResponses.update({_id: surveyResponseId}, { $set: { contentUrl: url }})
+        
+        if(!this.isSimulation){
+            //kick off the job to analyze video
+            new Job('surveyJobQueue', 'startVideoAnalysis',{
+                surveyResponseId,
+                url
+            })
+            .priority('normal')
+            .delay(60*60*1000)
+            .save()
+        }
     }
 })
 
