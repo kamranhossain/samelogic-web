8f7193381e3e2b93330a2d0328f90bc28b3aa271
Shawn Mclean <shawnrmclean@gmail.com>
2016-04-20 23:12:12 -0500

    removed icon property for emoji enum

--- imports/api/surveys/collections/emojis.js
+++ imports/api/surveys/collections/emojis.js
@@ -5,28 +5,24 @@ export default SEnum([
         key: 'smiling_face_with_heart_shaped_eyes',
         value: 1,
         label: 'Delighted',
-        icon: '',
         includeInSnapsSurvey: true        
     },
     {
         key: 'grinning_face',
         value: 2,
         label: 'Happy',
-        icon: '',
         includeInSnapsSurvey: true        
     },
     {
         key: 'neutral_face',
         value: 3,
         label: 'Meh',
-        icon: '',
         includeInSnapsSurvey: true        
     },
     {
         key: 'angry_face',
         value: 4,
         label: 'Unsatisfied',
-        icon: '',
         includeInSnapsSurvey: true        
     }
 ])
