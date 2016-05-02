bc2e908eab944f4d70a4f1d5f60e216b5074bf30
Shawn Mclean <shawnrmclean@gmail.com>
2016-05-01 23:09:54 -0500

    EmojiFeedback implemented.

---
+++ imports/ui/surveys/components/EmojiFeedback/EmojiFeedback.jsx
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class EmojiFeedback extends Component{
+    
+    render(){        
+        const { emojis }= this.props
+        
+        const selectedFeedback = emojis.data.find((e) => e.selected)
+        
+        let feedback = ''
+        if(selectedFeedback)
+            feedback = selectedFeedback.feedback
+                
+        return(
+            <div className="white-space-pre">
+                {feedback}
+            </div>
+        )
+    }
+}
+
+
+EmojiFeedback.propTypes = {
+    emojis: PropTypes.shape({
+        data: PropTypes.array,
+        selectedValue: PropTypes.number
+    })
+}
