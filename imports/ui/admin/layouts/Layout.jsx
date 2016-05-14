9f0f304a55a10c88ecbc06c6606e744ef7b673b0
Shawn Mclean <shawnrmclean@gmail.com>
2016-05-14 10:06:38 -0500

    refactoring layout for admin area

--- imports/ui/admin/layouts/Layout.jsx
+++ imports/ui/admin/layouts/Layout.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import Footer from '/imports/ui/admin/layouts/footer/Footer.jsx'
+import Header from '/imports/ui/admin/layouts/header/Header.jsx'
 
 export default class Layout extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.children}
-        <Footer />
-      </div>
-    )
-  }
+    render() {
+        return (
+          <div>
+            <Header />
+              {this.props.children}
+            <Footer />
+          </div>
+        )
+    }
 }
