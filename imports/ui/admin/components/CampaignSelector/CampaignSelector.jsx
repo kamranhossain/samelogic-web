219612e6c334fc2d2e4a1228860d178a43a82cb9
Shawn Mclean <shawnrmclean@gmail.com>
2016-05-15 12:46:37 -0500

    Added testing libs and CampaignSelector shell component.

---
+++ imports/ui/admin/components/CampaignSelector/CampaignSelector.jsx
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+export default class CampaignSelector extends Component{
+    constructor(props){
+        super(props)
+    }
+    render(){        
+        return (
+            <div>          
+            </div>
+        )
+    }
+}
+
+CampaignSelector.propTypes = {
+    campaigns: PropTypes.array
+}
