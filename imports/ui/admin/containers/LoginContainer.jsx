e0dbf3f02ac06220bdeaaefc7bae22a8f94aeef8
Shawn Mclean <shawnrmclean@gmail.com>
2016-05-16 19:48:17 -0500

    Added LoginContainer shell

---
+++ imports/ui/admin/containers/LoginContainer.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+export default class LoginContainer extends Component{    
+    constructor(props){
+        super(props)
+    }
+    
+    render(){
+        return( 
+            <div>
+            </div>
+        )
+    }
+}
