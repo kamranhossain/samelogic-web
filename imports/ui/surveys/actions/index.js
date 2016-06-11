cce93a5169449e7a1a071a0d53acd5a7adff0e40
Shawn Mclean <shawnrmclean@gmail.com>
2016-06-11 17:47:40 -0500

    Started adding new form snaps actions

--- imports/ui/surveys/actions/index.js
+++ imports/ui/surveys/actions/index.js
@@ -2,6 +2,7 @@ export * from './createVideoSurveyResponse'
 export * from './emojiSelected'
 export * from './snapSelected'
 export * from './surveys'
+export * from './snaps'
 
 export const RESET_ERROR_MESSAGE = 'RESET_ERROR_MESSAGE'
 
