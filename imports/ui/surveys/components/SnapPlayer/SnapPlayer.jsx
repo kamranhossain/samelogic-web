95c6082bcc99086a0373855d490b245c9991f1f4
Shawn Mclean <shawnrmclean@gmail.com>
2016-06-16 10:12:59 -0500

    Removed the duration from the video due to rendering complexities.

--- imports/ui/surveys/components/SnapPlayer/SnapPlayer.jsx
+++ imports/ui/surveys/components/SnapPlayer/SnapPlayer.jsx
@@ -1,37 +1,14 @@
 import React, { Component, PropTypes } from 'react'
 
 export default class SnapPlayer extends Component{
-    constructor(props){
-        super(props)
-
-        this.prevSnap = null
-        this.state = {
-            source: null,
-            duration: 0
-        }
-    }
-
-    videoMetaDataLoaded(event){
-        console.log(event)
-    }
-
-    render(){       
-        if(this.prevSnap != this.props.snap){
-            const fileReader = new FileReader()
-            fileReader.onload = (event) => { 
-                this.prevSnap = this.props.snap       
-                this.setState({
-                    source: event.target.result
-                })
-            }
-            fileReader.readAsDataURL(this.props.snap) 
-        }
+    render(){
+        const videoUrl = URL.createObjectURL(this.props.snap)
+        
         return (
             <div className="snap-player">
-                <video src={this.state.source} preload="metadata" loadeddata={(event) => console.log(event)}>
+                <video src={videoUrl} ref="video" preload="metadata">
                     <p>Your browser does not support the HTML5 Video element.</p>
-                </video>
-                <div className="SnapDuration"><b>{this.state.duration}s</b></div>
+                </video>                
             </div>
         )
     }
