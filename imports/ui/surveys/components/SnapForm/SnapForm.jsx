82abbbe191c793b186dd254018aabf5c8bd3395b
Shawn Mclean <shawnrmclean@gmail.com>
2016-06-15 20:43:21 -0500

    Fixed Snap Player to reload videos when re-selected.

--- imports/ui/surveys/components/SnapForm/SnapForm.jsx
+++ imports/ui/surveys/components/SnapForm/SnapForm.jsx
@@ -5,13 +5,12 @@ import EmojiSelector from '/imports/ui/surveys/components/EmojiSelector/EmojiSel
 import SnapSelector from '/imports/ui/surveys/components/SnapSelector/SnapSelector.jsx'
 import SnapPlayer from '/imports/ui/surveys/components/SnapPlayer/SnapPlayer.jsx'
 import SurveyorIdentity from '/imports/ui/surveys/components/SurveyorIdentity/SurveyorIdentity.jsx'
-import EmojiFeedback from '/imports/ui/surveys/components/EmojiFeedback/EmojiFeedback.jsx'
 
 class SnapForm extends Component {
 
     render() {
-        const { asyncValidating, fields: { emoji, snap, comment }, handleSubmit, submitting, submitSnap, actions, newSnap, survey, emojis } = this.props
-
+        const { asyncValidating, fields, handleSubmit, submitting, submitSnap, actions, newSnap, survey, emojis } = this.props
+        const { emoji, snap, comment } = fields
         return (
             <div className="snap-form">
                 {survey.current && survey.ready ?
@@ -24,6 +23,26 @@ class SnapForm extends Component {
                     </div>                    
                     <div className="row emoji-selector-container">
                         <EmojiSelector options={emojis.items} {...emoji} />
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            {snap.valid ? 
+                                <SnapPlayer snap={snap.value[0]} /> : 
+                                <div className="medium-lite-txt lmt lmb">Tell us about your experience in <strong>15 - 16 seconds</strong>!</div>
+                            }
+                        </div>
+                    </div>
+                    <div className="row action-button-container">
+
+                        <div className="snap-selector">
+                            <div className="btn btn-file btn-disc"> 
+                                <input type="file" {...snap} value={null} />
+                                <span className="inner center-block red-bg">
+                                    <i className="glyphicon glyphicon-camera white" />
+                                </span>
+                            </div>
+                        </div>
+
                     </div>
                 </form> : null
                 }
--- imports/ui/surveys/components/SnapPlayer/SnapPlayer.jsx
+++ imports/ui/surveys/components/SnapPlayer/SnapPlayer.jsx
@@ -3,29 +3,32 @@ import React, { Component, PropTypes } from 'react'
 export default class SnapPlayer extends Component{
     constructor(props){
         super(props)
-        this.snap = props.snap
-        
+
+        this.prevSnap = null
         this.state = {
             source: null,
             duration: 0
         }
     }
-    
-    componentDidMount(){
-        const fileReader = new FileReader()
-        fileReader.onload = (event) => {        
-            this.setState({
-                source: event.target.result,
-                duration: Math.floor(this.snap.duration)
-            })
+
+    videoMetaDataLoaded(event){
+        console.log(event)
+    }
+
+    render(){       
+        if(this.prevSnap != this.props.snap){
+            const fileReader = new FileReader()
+            fileReader.onload = (event) => { 
+                this.prevSnap = this.props.snap       
+                this.setState({
+                    source: event.target.result
+                })
+            }
+            fileReader.readAsDataURL(this.props.snap) 
         }
-        fileReader.readAsDataURL(this.snap.data)        
-    }    
-    
-    render(){        
         return (
             <div className="snap-player">
-                <video src={this.state.source}>
+                <video src={this.state.source} preload="metadata" loadeddata={(event) => console.log(event)}>
                     <p>Your browser does not support the HTML5 Video element.</p>
                 </video>
                 <div className="SnapDuration"><b>{this.state.duration}s</b></div>
