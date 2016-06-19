246bf9773c6d50afe5c1ffeb55932171d92967d8
Shawn Mclean <shawnrmclean@gmail.com>
2016-06-19 17:43:41 -0500

    Added some more text to SurveyNotFound. [Finishes #121802579]

--- imports/ui/surveys/components/SurveyNotFound/SurveyNotFound.jsx
+++ imports/ui/surveys/components/SurveyNotFound/SurveyNotFound.jsx
@@ -5,6 +5,9 @@ class SurveyNotFound extends Component {
         return (
             <div>
                 <h1>Survey Not Found!</h1>
+                <div>
+                    Please contact the person you've received this link from.
+                </div>
             </div>
         )
     }
