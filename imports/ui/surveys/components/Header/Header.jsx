14d17bd77ea375d9c2d6387169e22a735ca5c281
Shawn Mclean <shawnrmclean@gmail.com>
2016-06-19 14:28:35 -0500

    Progress Bar autoincrement is now determined by loading percent.

--- imports/ui/surveys/components/Header/Header.jsx
+++ imports/ui/surveys/components/Header/Header.jsx
@@ -5,15 +5,18 @@ class Header extends Component {
     render() {
         const { loading, loadingPercent } = this.props
         let percent = 100
+        let autoIncrement = true
 
-        if(loading && loadingPercent > 0)
+        if(loading && loadingPercent > 0){
             percent = loadingPercent
+            autoIncrement = false
+        }
         else if(loading)
             percent = 1
 
         return (
             <div>               
-                <ProgressBar onTop={true} autoIncrement={true} percent={percent} />
+                <ProgressBar onTop={true} autoIncrement={autoIncrement} percent={percent} />
             </div>
         )
     }
