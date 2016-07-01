58714be0f49e617382066743abf6edf96469983b
Shawn Mclean <shawnrmclean@gmail.com>
2016-07-01 01:43:08 -0500

    added dummy account on app start

--- client/main.js
+++ client/main.js
@@ -1,7 +1,15 @@
 import { Meteor } from 'meteor/meteor'
+import {Accounts} from 'meteor/accounts-base'
 import { render } from 'react-dom'
 import { renderRoutes } from '/imports/startup/client/routes.jsx'
 
 Meteor.startup(() => {
     render(renderRoutes(), document.getElementById('app'))
+
+    if (Meteor.users.find().count() === 0) {
+        Accounts.createUser({
+            email: 'test@example.com',
+            password: 'password'
+        })
+    }
 })
