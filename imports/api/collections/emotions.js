a555d7e47fc3bc6d02ffcc0123861a592f0bb5a2
Shawn Mclean <shawnrmclean@gmail.com>
2016-07-22 20:33:06 -0500

    emotions listed out

---
+++ imports/api/collections/emotions.js
@@ -0,0 +1,44 @@
+import { SEnum } from 'meteor/zeroasterisk:s-enum'
+
+export default SEnum([
+    {
+        key: 'happiness',
+        value: 1,
+        label: 'Happiness'
+    },
+    {
+        key: 'surprise',
+        value: 2,
+        label: 'Surprise'
+    },
+    {
+        key: 'contempt',
+        value: 3,
+        label: 'Contempt'
+    },
+    {
+        key: 'neutral',
+        value: 4,
+        label: 'Neutral'
+    },
+    {
+        key: 'sadness',
+        value: 5,
+        label: 'Sadness'
+    },
+    {
+        key: 'disgust',
+        value: 6,
+        label: 'Disgust'
+    },
+    {
+        key: 'anger',
+        value: 7,
+        label: 'Anger'
+    },
+    {
+        key: 'fear',
+        value: 8,
+        label: 'Fear'
+    }
+])
