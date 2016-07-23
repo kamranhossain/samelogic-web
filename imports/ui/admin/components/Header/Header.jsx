e477564fe223290d7ab8f84182ca7907147ab8e5
Shawn Mclean <shawnrmclean@gmail.com>
2016-07-22 23:36:29 -0500

    Navbar is using react-bootstrap. [Ref #121802133]

--- imports/ui/admin/components/Header/Header.jsx
+++ imports/ui/admin/components/Header/Header.jsx
@@ -1,35 +1,39 @@
 import React, {Component, PropTypes} from 'react'
 import { Link } from 'react-router'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 
 class Header extends Component {
     render() {
         const {user, actions} = this.props
 
         return (
-            <nav className="header navbar navbar-default navbar-static-top">
-                <div className="container">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a className="navbar-brand" href="#">Samelogic</a>
-                    </div>
-
-                    <div id="navbar" className="navbar-collapse collapse">
-                        <ul className="nav navbar-nav">
-                            <li><Link to="/admin">Dashboard</Link></li>
-                            <li><Link to="/admin/campaigns">Campaigns</Link></li>                            
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-
-                            <li><a onClick={actions.logout} href="#">Sign Out</a></li>
-                        </ul>
-                    </div>
-                </div>    
-            </nav>
+            <Navbar staticTop>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">Samelogic</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer to="/admin">
+                            <NavItem eventKey={1}>Dashboard</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/admin/campaigns">
+                            <NavItem eventKey={2}>Campaigns</NavItem>
+                        </LinkContainer>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavDropdown eventKey={3} title="Account" id="nav-dropdown">
+                            <MenuItem eventKey={3.1}>Profile</MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.2}>Settings</MenuItem>
+                            <MenuItem eventKey={3.3} onClick={actions.logout}>Sign Out</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>                
+            </Navbar>
         )
     }
 }
